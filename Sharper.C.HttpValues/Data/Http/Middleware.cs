15bf8d9c1581d3755bd4367938947e561207b770
Jeremy Hughes <jedahu@gmail.com>
2016-06-23 09:25:44 +1200

    Remove unused Middleware definition

--- Sharper.C.HttpValues/Data/Http/Middleware.cs
+++ Sharper.C.HttpValues/Data/Http/Middleware.cs
@@ -3,20 +3,11 @@ namespace Sharper.C.Data.Http
     public delegate Handler<B, J>
     Middleware<B, A, J, I>(Handler<A, I> ab);
 
-    public delegate Handler<J>
-    Middleware<J, I>(Handler<I> h);
-
     public static class Middleware
     {
         public static Middleware<C, A, K, I> Then<C, B, A, K, J, I>
           ( this Middleware<C, B, K, J> m2
           , Middleware<B, A, J, I> m1
-          )
-        =>  h => m2(m1(h));
-
-        public static Middleware<K, I> Then<K, J, I>
-          ( this Middleware<K, J> m2
-          , Middleware<J, I> m1
           )
         =>  h => m2(m1(h));
     }
